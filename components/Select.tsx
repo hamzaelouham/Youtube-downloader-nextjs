40e1d252b72dff9b4a14891478fd81bd5f55ba68
hamza elouham <hamzaelouham@gmail.com>
2022-08-08 16:09:34 +0100

    finalze

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-import axios from "axios";
-import downloader from "downloadjs";
+import React, { useState } from "react";
 
 interface Props {
   url: string;
@@ -17,7 +15,7 @@ interface Props {
 }
 
 export default function Select({ formats, url }: Props) {
-  const [options, setOptions] = React.useState<string>("135");
+  const [options, setOptions] = useState<string>("135");
 
   const download = () => {
     window.location.href = `${process.env.NEXT_API_BASE_URL}/download?url=${url}?options=${options}`;

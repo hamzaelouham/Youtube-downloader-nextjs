e1ae033cf2048452df7a9e967f46f8855eab2d95
hamza elouham <hamzaelouham@gmail.com>
2022-08-11 12:11:44 +0100

    optimazing seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/style.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>downlader</title>
+        <meta
+          name="description"
+          content="downlaod your favorite youtute videos faster"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
 import Main from "../components/Main";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>downlader</title>
-      </Head>
       <Header />
       <Main />
     </div>
